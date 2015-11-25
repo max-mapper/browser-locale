a99d49373a136c71bf2bb1621e38817076c89ea6
Carsten <acidicX@users.noreply.github.com>
2015-11-25 14:41:12 +0100

    Check navigator.languages array for entries
    Otherwise this method returns undefined for Opera Mobile on Android with only one language.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ module.exports = function() {
     // chrome does not currently set navigator.language correctly https://code.google.com/p/chromium/issues/detail?id=101138
     // but it does set the first element of navigator.languages correctly
     lang = navigator.languages[0]
+    if (!lang) {
+      // chrome mobile otherwise returns undefined
+      lang = navigator.language
+    }
   } else if (navigator.userLanguage) {
     // IE only
     lang = navigator.userLanguage
