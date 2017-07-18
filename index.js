ba3ab8895e349808e1434d90ab7bb74cb3b568e0
Oana Maria Teodorescu <teodorescu_oanamaria@yahoo.com>
2017-07-18 13:17:42 +0300

    Check browserLanguage before languages array

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ module.exports = function browserLocale () {
   } else if (navigator.language) {
     // latest versions of Chrome, Firefox, Edge and Safari set this correctly
     lang = navigator.language;
-  } else if (navigator.languages && navigator.languages.length) {
-    // latest versions of Chrome and Firefox set this correctly
-    lang = navigator.languages[0];
   } else if (navigator.browserLanguage) {
     // used by Edge
     lang = navigator.browserLanguage;
+  } else if (navigator.languages && navigator.languages.length) {
+    // latest versions of Chrome and Firefox set this correctly
+    lang = navigator.languages[0];
   }
 
   return lang;
